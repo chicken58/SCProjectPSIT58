62d3213f1243d5d750611a00b9b655f16bc99ad5
bdloner <plumeria.in.sky@gmail.com>
2015-12-04 14:20:02 +0700

    Python

---
+++ BrickBridge.py
@@ -0,0 +1,11 @@
+"""BrickBridge"""
+def brick_bridge(bridge, brick_l, brick_s):
+    """a"""
+    ans1 = bridge//5
+    if ans1 > brick_l:
+        ans1 = brick_l
+    if brick_s - ans1*5 > bridge:
+        print("-1")
+    else:
+        print(brick_s-(ans1*5))
+brick_bridge(int(input()), int(input()), int(input()))
