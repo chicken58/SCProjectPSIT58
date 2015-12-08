4bf9d3fd62def505bca45e9767815a277eda2d92
Mr.Yosapat Thamarongrak <plumeria.in.sky@gmail.com>
2015-12-08 13:26:45 +0700

    Change Graph

--- plotly.py
+++ plotly.py
@@ -1,39 +1,67 @@
+# Learn about API authentication here: https://plot.ly/python/getting-started
+# Find your api_key here: https://plot.ly/settings/api
+
 import plotly.plotly as py
-from plotly.graph_objs import *
-py.sign_in('emptygiiiz', 'dmughevmvv')
-trace1 = Bar(
+import plotly.graph_objs as go
+
+trace1 = go.Bar(
     x=[2548, 2549, 2550, 2551, 2552, 2553, 2554, 2555, 2556, 2557],
     y=[99.56, 93.38, 95.6, 93.96, 94.45, 93.65, 95.37, 97.56, 97.88, 97.08],
-    name='',
-    uid='743f82'
-)
-trace2 = Bar(
-    x=[2548, 2549, 2550, 2551, 2552, 2553, 2554, 2555, 2556, 2557],
-    y=[29.11, 24.08, 25.73, 27.5, 26.93, 26.41, 27.54, 28.79, 26.08, 27.13],
-    name='',
-    uid='b1b664'
-)
-trace3 = Bar(
-    x=[2548, 2549, 2550, 2551, 2552, 2553, 2554, 2555, 2556, 2557],
-    y=[128.67, 117.45, 121.33, 121.46, 121.38, 120.06, 122.91, 126.35, 123.96, 124.21],
-    name='',
-    uid='4f5d72'
-)
-data = Data([trace1, trace2, trace3])
-layout = Layout(
-    autosize=True,
-    height=536,
-    width=1121,
-    xaxis=XAxis(
-        autorange=True,
-        range=[2547.5, 2557.5],
-        type='linear'
-    ),
-    yaxis=YAxis(
-        autorange=True,
-        range=[0, 135.44210526315788],
-        type='linear'
+    name='Man',
+    marker=dict(
+        color='rgb(55, 83, 109)'
     )
 )
-fig = Figure(data=data, layout=layout)
-plot_url = py.plot(fig)
+trace2 = go.Bar(
+    x=[2548, 2549, 2550, 2551, 2552, 2553, 2554, 2555, 2556, 2557],
+    y=[29.11, 24.08, 25.73, 27.5, 26.93, 26.41, 27.54, 28.79, 26.08, 27.13],
+    name='Woman',
+    marker=dict(
+        color='rgb(26, 118, 255)'
+    )
+)
+trace3 = go.Bar(
+    x=[2548, 2549, 2550, 2551, 2552, 2553, 2554, 2555, 2556, 2557],
+    y=[128.67, 117.45, 121.33, 121.46, 121.38, 120.06, 122.91, 126.35, 123.96, 124.21],
+    name='Total',
+    marker=dict(
+        color='rgb(13, 158, 188)'
+    )
+)
+data = [trace1, trace2, trace3]
+layout = go.Layout(
+    title='กราฟแท่งแสดงยอดผู้เสียชีวิตจากการฆ่าตัวตายในประเทศไทยตั้งแต่ พ.ศ.2548 - 2557',
+    xaxis=dict(
+        title='จำนวนปีตั้งแต่ พ.ศ.2548 - 2557',
+        titlefont=dict(
+            size=16,
+            color='rgb(107, 107, 107)'
+        ),
+        tickfont=dict(
+            size=14,
+            color='rgb(107, 107, 107)'
+        )
+    ),
+    yaxis=dict(
+        title='จำนวนคนฆ่าตัวตายต่อประชากรล้านคน',
+        titlefont=dict(
+            size=16,
+            color='rgb(107, 107, 107)'
+        ),
+        tickfont=dict(
+            size=14,
+            color='rgb(107, 107, 107)'
+        )
+    ),
+    legend=dict(
+        x=0,
+        y=1.0,
+        bgcolor='rgba(255, 255, 255, 0)',
+        bordercolor='rgba(255, 255, 255, 0)'
+    ),
+    barmode='group',
+    bargap=0.15,
+    bargroupgap=0.1
+)
+fig = go.Figure(data=data, layout=layout)
+plot_url = py.plot(fig, filename='style-bar')
