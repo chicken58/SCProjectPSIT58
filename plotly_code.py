644ff470848688a2271e8aba6d9d98a5e3dc78bc
surasee1234 <doppelganger_2011@hotmail.com>
2015-12-12 13:54:28 +0700

    Rename 2548.py to process.py and update description of plotly_code.py

--- plotly_code.py
+++ plotly_code.py
@@ -9,16 +9,21 @@ import plotly.plotly as py
 import plotly.graph_objs as go
 
 def put_input():
-    print("How to input?\nEx.\n    1. 2548-2552\n    2. 2550-2557\n    3. All\n    **(1. & 2.\
-The range of input have to more then 2547 and less than 2558.)**\n    **(3. If your input \
-is\All, the result will display 2548-2557 and predict of 2558.)**")
-    the_input = input("Please put your input. ")
-    if the_input == "All":
-        return ['2548', '2549', '2550', '2551', '2552', '2553', '2554', '2555', '2556', '2557', '2558']
-    else:
-        first, last = the_input.split("-")
-        lis = [str(i) for i in range(int(first), int(last)+1)]
-        return lis
+    print("How to input?\nEx.\n    1. 2548-2552\n    2. 2550-2557\n    3. 2557-2557\n    4. All\n    **\
+(1. & 2. The range of input have to more then 2547 and less than 2558.)**\n    **(3. Show one \
+year.)**\n    **(4. If your input is\All, the result will display 2548-2557 and predict of 2558.)**")
+    while True:
+        the_input = input("Please put your input : ")
+        if the_input == "all" or the_input == "All" or the_input == "ALL":
+            return ['2548', '2549', '2550', '2551', '2552', '2553', '2554', '2555', '2556', '2557', '2558']
+        elif "-" not in the_input:
+            print("Your input is invalid, plase put your input again.")
+        else:
+            first, last = the_input.split("-")
+            if 2548 <= int(first) <= 2557 and 2548 <= int(last) <= 2557 and int(last) >= int(first):
+                lis = [str(i) for i in range(int(first), int(last)+1)]
+                return lis
+            else: print("Your input is invalid, plase put your input again.")
 
 def call_from_result():
     wb = load_workbook('result.xlsx')
@@ -33,8 +38,19 @@ def call_from_result():
                 total_lis.append(float(sh['F'+str(j)].value))
     return man_lis, woman_lis, total_lis, year
 
+def title(year_range):
+    if year_range == [str(i) for i in range(2548, 2559)]:
+        pre_title = "since 2548-2557 and predict of 2558."
+    else:
+        first, last = year_range[0], year_range[-1]
+        pre_title = "since "+ first + "-" + last + "."
+    title_1 = "Bar graphs show total deaths from suicide in Thailand " + pre_title
+    title_2 = "Total " + pre_title
+    return title_1, title_2
+
 def make_a_graph():
     man_lis, woman_lis, total_lis, year_range = call_from_result()
+    title_1, title_2 = title(year_range)
     trace1 = go.Bar(
         x=year_range,
         y=man_lis,
@@ -62,9 +78,9 @@ def make_a_graph():
     )
     data = [trace1, trace2, trace3]
     layout = go.Layout(
-        title='Bar graphs show total deaths from suicide in Thailand since 2548-2557 and predict of 2558.',
+        title=title_1,
         xaxis=dict(
-            title='Total since 2548 - 2557 and predict of 2558.',
+            title=title_2,
             titlefont=dict(
                 size=16,
                 color='rgb(107, 107, 107)'
@@ -99,3 +115,4 @@ def make_a_graph():
     plot_url = py.plot(fig, filename='style-bar')
 
 make_a_graph()
+
